9892cce6d44c3fb39a9f1015b7f32c9ffc7cc2c7
Nana Kwaku <nksarps@gmail.com>
2026-02-18 10:32:11 +0000

    fix: fixed assertion for tc001

--- src/test/java/com/automation/tests/NewsletterTest.java
+++ src/test/java/com/automation/tests/NewsletterTest.java
@@ -52,7 +52,7 @@ public class NewsletterTest extends BaseTest {
 
         page.submitWithEmail(email);
 
-        assertEquals("Thanksfor subscribing!", page.getSuccessTitle());
+        assertEquals("Thanks for subscribing!", page.getSuccessTitle());
         assertEquals(email, page.getSuccessEmail());
     }
 
